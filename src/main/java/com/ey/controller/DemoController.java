c70fd34263396c055f5f0523ae6a25f4048b825a
user12 <23122002deepti@gmail.com>
2026-01-07 12:49:48 +0530

    second commit

--- src/main/java/com/ey/controller/DemoController.java
+++ src/main/java/com/ey/controller/DemoController.java
@@ -1,5 +1,14 @@
 package com.ey.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DemoController {
 
+	@GetMapping
+	public String home() {
+		return "Welcome  Spring home";
+	}
+	
 }
